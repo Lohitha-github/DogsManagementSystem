c0178c153bec7617c51b73654c4728f153d2ef8d
Lohitha-github <111551663+Lohitha-github@users.noreply.github.com>
2023-04-30 13:56:12 -0500

    first commit

---
+++ src/main/java/com/bandiClasses/DMS/DogsManagementSystemsApplication.java
@@ -0,0 +1,20 @@
+package com.bandiClasses.DMS;
+/**
+ * 
+ * @author Lohitha Julakanti(s555815)
+ *
+ */
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DogsManagementSystemsApplication {
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(DogsManagementSystemsApplication.class, args);
+	}
+
+	
+}
